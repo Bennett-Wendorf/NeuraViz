73bd7a9d0d15fa981e59c549b0ab87fb85f8f1e2
Bennett-Wendorf <bennettwendorf@gmail.com>
2023-07-04 16:02:14 -0500

    Finished up first action iteration and build script
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

---
+++ src/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get latest changes
+git checkout main
+git pull
+
+# Cleanup old build files
+rm -rf ./src/frontend/public/build
+
+# Ensure frontend dependencies all exist
+cd frontend
+npm install
+cd ..
+
+cd backend
+poetry install
+cd ..
+
+# Build frontend
+cd frontend
+npm run build
+cd ..
+
+# Restart the application
+pm2 restart MSE_Capstone
